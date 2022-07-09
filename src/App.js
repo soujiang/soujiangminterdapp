9ec0aab3e8df1a8e53c88254fdef56285f9dc3d7
soujiang <103637149+soujiang@users.noreply.github.com>
2022-07-09 06:15:55 -0400

    app 20%

--- src/App.js
+++ src/App.js
@@ -408,7 +408,7 @@ function App() {
           </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
-        <s.Container jc={"center"} ai={"center"} style={{ width: "90%" }}>
+        <s.Container jc={"center"} ai={"center"} style={{ width: "20%" }}>
           <s.TextDescription
             style={{
               textAlign: "center",
